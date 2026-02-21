cf72c2a28652e8d36452f1be0522c72fbd3c7e96
erhanketen <ketenerhan2006@gmail.com>
2026-02-21 22:11:51 +0300

    Add files via upload

--- calculator_v3.py
+++ calculator_v3.py
@@ -1,25 +1,37 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-while True:
+def islemler():
     girdiler = []
     while True:
-        girdi = input("İşlemi Giriniz (Bitirmek için '='):")
+        girdi = input("İşlem (çıkmak için =):")
         if girdi == "=":
             break
         else:
             girdiler.append(girdi)
-    break
+    girdilerint = []
+    for i in girdiler:
+        try:
+            i = int(i)
+            girdilerint.append(i)
+        except ValueError:
+            girdilerint.append(i)
+    for i in girdilerint:
+        if i == "+":
+            girdilerint[girdilerint.index(i)+1] = girdilerint[girdilerint.index(i)+1] + girdilerint[girdilerint.index(i)-1]
+            girdilerint.pop(girdilerint.index(i))
+        elif i == "-":
+            girdilerint[girdilerint.index(i) + 1] = girdilerint[girdilerint.index(i)-1] - girdilerint[girdilerint.index(i)+1]
+            girdilerint.pop(girdilerint.index(i))
+    return girdilerint[-1]
+
+
+print(islemler())
+
+
+
+
+
+
+
+
 
 
 
